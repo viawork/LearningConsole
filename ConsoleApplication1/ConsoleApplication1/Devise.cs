5cfed7ac103aa0f6d94fdfbb87e31898e85410ed
viawork <41791593+viawork@users.noreply.github.com>
2019-10-28 12:16:11 +0200

    new classs

---
+++ ConsoleApplication1/ConsoleApplication1/Devise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Devise
+    {
+        string type;
+        int memory { get; set; }
+        public object Messagebox { get; private set; }
+
+        int storage;
+        string processor;
+        public void ShowType()
+        {
+            Messagebox
+        }
+
+    }
+}
